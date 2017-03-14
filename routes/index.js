808ad97d801072a44c3a24e20d1bb0741a20cd85
PatrickBrouder <K00203617@student.lit.ie>
2017-03-14 12:40:33 +0000

    5

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,33 @@ router.get('/', function(req, res, next) {
 router.get('/create', function(req, res, next) {
   res.render('createAccount');
 });
+
+router.post('/newAccount', function(req, res, next){
+  var dbConnection= mysql.createConnection(dbConnectionInfo);
+  dbConnection.connect();
+
+  dbConnection.on('error', function(err){
+    if(err.code == 'PROTOCOL_SEQUENCE_TIMEOUT'){
+      console.log('Got a PROTOCOL_SEQUENCE_TIMEOUT')
+    } else {
+      console.log('Got a db error ', err);
+    }
+  });
+
+  var userInfo={}
+  userInfo.username= req.body.username;
+  userInfo.email= req.body.email;
+  userInfo.password= req.body.password;
+  dbConnection.query('INSERT INTO user(username, email, password) VALUES(?,?, ?)',[userInfo.username, userInfo.email, userInfo.password], function(err,results,fields){
+
+      if(err){
+          throw err;
+      }
+     // listItem.id = results.insertId;
+      dbConnection.end();
+      res.redirect('/home');
+  });
+
+
+});
 module.exports = router;
