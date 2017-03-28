8e09a4ebd44d8f33c14da0558f572dab3c2527f7
PatrickBrouder <K00203617@student.lit.ie>
2017-03-28 14:06:34 +0100

    13

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,10 @@ var dbConnectionInfo = {
   password : 'dde1f314',
   database : 'acsm_c027cee5201f6e7'
 };
-//req.session.errorMessage ="";
+req.session.errorMessage ="";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{ errorMsg: req.session.errorMessage });
 });
 router.get('/create', function(req, res, next) {
   res.render('createAccount');
