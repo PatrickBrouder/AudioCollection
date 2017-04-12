1a23705d6622d631e21bc2dc2073fcae96b5ac6f
PatrickBrouder <K00203617@student.lit.ie>
2017-04-12 17:02:34 +0100

    57

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 
-
+/*
 var dbConnectionInfo = {
   host : 'localhost',
   user : 'root',
   password : 'Liverbuda1',
   database : 'audio'
 }
+*/
+var dbConnectionInfo = {
+  host : 'eu-cdbr-azure-west-d.cloudapp.net',
+  user : 'b7ac63e92a8598',
+  password : 'dde1f314',
+  database : 'acsm_c027cee5201f6e7'
+};
 
 router.get('/', function(req, res, next) {
   res.render('index');
